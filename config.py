56c51230468ad8e1515bc54121dda5b304f8c5b3
Prakash Monis <Prakashmonis06@gmail.com>
2025-11-07 11:57:38 +0530

    link

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = 'your_secret_key'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/expense_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://expense_db_kwzv_user:HHdViICo7yR23jSTDyfkXUlERQofP1PP@dpg-d46p1124d50c7390v350-a/expense_db_kwzv'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
