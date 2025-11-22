e9d421ab0841419d1284bdd7214310a441c75b94
Prakash Monis <Prakashmonis06@gmail.com>
2025-11-22 11:16:54 +0530

    neon ready

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = 'your_secret_key'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'postgresql://expense_db_kwzv_user:HHdViICo7yR23jSTDyfkXUlERQofP1PP@dpg-d46p1124d50c7390v350-a/expense_db_kwzv'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_A4lXSzJU7GxR@ep-little-frost-a4t0ihy2-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
